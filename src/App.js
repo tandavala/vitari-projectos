c22ac897371c346711927b1cc25c46e95da2b3be
tandavala <jose.tandavala@gmail.com>
2020-01-04 00:13:26 +0100

    finishing version 1 y

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/layout/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
