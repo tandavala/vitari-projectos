4a8bff46141f6e86cff3d037a6211d8063ee63ab
tandavala <jose.tandavala@gmail.com>
2020-01-04 15:21:53 +0100

    create a firebase cloud function for notification

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,36 @@
 const functions = require("firebase-functions");
 
+// to access firestore with functions
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
 // generate a function indicating a message
 exports.firstFunction = functions.https.onRequest((request, response) => {
   response.send("Hello Tandavala");
 });
+
+// Adds documents to the collection
+const createNotification = notification => {
+  // we are adding data to the notification
+  return admin
+    .firestore()
+    .collection("notifications")
+    .add(notification)
+    .then(doc => console.log("adicionado uma notificação ", doc));
+};
+// functions that controls projects collection
+exports.projectCreated = functions.firestore
+  .document("projects/{projectId}")
+  .onCreate(doc => {
+    // Access the added project
+    const project = doc.data();
+    // Create the notification object
+    const notity = {
+      content: "Novo Projecto foi criado",
+      user: `${project.authorNome} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    // Add notitication object
+    return createNotification(notity);
+  });
