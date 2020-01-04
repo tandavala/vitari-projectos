396b63124b7987aa5ed0940b89b63e0db66d1575
tandavala <jose.tandavala@gmail.com>
2020-01-04 08:09:59 +0100

    coding the initial Dashboard and Notifications components

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import ProjectList from "../Projects/ProjectList";
+import Notifications from "./Notifications";
 
-function Dashboard(props) {
-  return <div>Dashboard Component</div>;
+import { connect } from "react-redux";
+
+class Dashboard extends Component {
+  render() {
+    const { projects } = this.props;
+
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList projects={projects} />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Dashboard;
+const mapStateToProps = state => {
+  return {
+    projects: state.projects.projects
+  };
+};
+export default connect(mapStateToProps)(Dashboard);
