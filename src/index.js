3f1b0b0f2c92cbee6499f84fa1424a41b70ed29e
tandavala <jose.tandavala@gmail.com>
2020-01-04 11:38:03 +0100

    refactoring redux configuration

--- src/index.js
+++ src/index.js
@@ -3,25 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 
-import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import { reduxFirestore, getFirestore } from "redux-firestore";
-
-import rootReducer from "./store/reducers/rootReducer";
-
-import firebaseConfig from "./config/firebaseConfig";
-
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reduxFirestore(firebaseConfig),
-    reduxFirestore(firebaseConfig)
-  )
-);
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import firebase from "../config/firebaseConfig";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+
+import rootReducer from "./reducers";
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+  attachAuthIsReady: true // attaches auth is ready promise to store
+};
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase),
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+  )
+);
+export default store;
