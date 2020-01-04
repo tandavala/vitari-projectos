8d7485b083fc7ed27f433d7bfee43a0a57dcbb5e
tandavala <jose.tandavala@gmail.com>
2020-01-04 12:51:52 +0100

    adding the auth compoents

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -1,4 +1,25 @@
 import React from "react";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div className="container">
+      <form className="white">
+        <h5 className="grey-text text-darken-3">LogIn</h5>
+        {/* for email */}
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        {/* for password */}
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        {/* for submit */}
+        <div className="input-field">
+          <button className="btn green lighten-1 z-depth-0">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/Components/Auth/SignUp.js
+++ src/Components/Auth/SignUp.js
@@ -1,5 +1,34 @@
 import React from "react";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="container">
+      <form className="white">
+        <h5 className="grey-text text-darken-3">Sign Up</h5>
+        {/* for email in auth */}
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        {/* for password in auth */}
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        {/* for first name in firestore */}
+        <div className="input-field">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" />
+        </div>
+        {/* for last name in firestore */}
+        <div className="input-field">
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" />
+        </div>
+        <div className="input-field">
+          <button className="btn blue lighten-1 z-depth-0">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
 }
