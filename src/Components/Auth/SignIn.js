085b411d45141f8f67c8ea40ed6bdaef023c044b
tandavala <jose.tandavala@gmail.com>
2020-01-04 07:57:18 +0100

    coding new auth components

---
+++ src/Components/Auth/SignIn.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function SignIn() {
+  return <div>SignIn</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./Components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import rootReducer from "./store/reducers/rootReducer";
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
