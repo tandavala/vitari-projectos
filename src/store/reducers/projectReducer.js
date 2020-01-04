60dc5a8e87a61e76d1b5b6c8cbe39e4fe6200685
tandavala <jose.tandavala@gmail.com>
2020-01-04 09:30:51 +0100

    coding the project reducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -8,6 +8,10 @@ const initState = {
 };
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("Create project", action.project);
+  }
   return state;
 };
 
